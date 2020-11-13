74bd37e5ef5100c873cb03047fb11f9a6742e878
asyncdeveloper <contact@asyncdeveloper.com>
2020-11-13 16:02:22 +0100

    Writing doc

--- lib/hello_elixir.ex
+++ lib/hello_elixir.ex
@@ -1,18 +1,58 @@
 defmodule HelloElixir do
   @moduledoc """
-  Documentation for `HelloElixir`.
+    Provides methods for handling a deck of cards
   """
 
   @doc """
-  Hello world.
+    create a deck of cards
+  """
+  def createDeck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suites = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suite <- suites do
+        "#{value} of #{suite}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+    `handSize` indicates how many hands to return
 
   ## Examples
-
-      iex> HelloElixir.hello()
-      :world
-
+        iex> deck = HelloElixir.createDeck
+        iex> { hand, deck } = HelloElixir.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
   """
-  def hello do
-    :world
+  def deal(deck, handSize) do
+    Enum.split(deck, handSize)
   end
+
+  def save(deck, fileName) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(fileName, binary)
+  end
+
+  def load(fileName) do
+    case File.read(fileName) do
+      { :ok, binary } -> :erlang.binary_to_term(binary)
+      { :error, _reason } ->  "File not found."
+    end
+  end
+
+  def createHand(size) do
+    HelloElixir.createDeck
+    |> HelloElixir.shuffle
+    |> HelloElixir.deal(size)
+  end
+
 end
